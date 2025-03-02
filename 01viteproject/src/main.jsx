c59f55b650d0757ab0d3f02d8a7b883d65834d90
Ajeet kumar <akuma246@partner.jaguarlandrover.com>
2025-03-02 17:25:27 +0530

    changes

--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+  return(
+    <h1>
+      Hello Ajeet
+    </h1>
+  )
+}
+
+const customElement={
+  type:'a',
+  props:{
+      href:'https://google.com',
+      target:'_blank',
+       },
+       children:'Click me to visit Google'
+
+}
+
+const anotherElement="Hii"
+const reactElement=React.createElement(
+  'a',
+  {href:"http://google.com",target:"_blank"},
+  'click me to visit site',
+  anotherElement
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+   // customElement  // it will not render because in there 
+                      // is some predefined attributes is there so our given attribute will not work
+
+    reactElement                 
 )
